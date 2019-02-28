2172f5633ac0563f9bb13a7680b0705a6099cfd6
lestat <lestat@lestat.me>
2019-02-28 12:34:16 +0800

    修改了项目结构

---
+++ openresty/get_args.lua
@@ -0,0 +1,14 @@
+-- local cjson = require "cjson"
+
+-- test = {}
+
+-- test['name'] = 'lestat'
+-- test['age'] = '18'
+
+-- local data = ngx.req.get_body_data()
+
+-- ngx.header['Api-Version'] = 'v1'
+-- ngx.header['Api-Powered-By'] = 'zmartec'
+-- ngx.header['Server'] = cjson.encode(test)
+
+--res = ngx.location.capture('/test',{ method = ngx.HTTP_POST, body = 'hello, world' })
