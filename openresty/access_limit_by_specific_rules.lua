8b1e5285b90379feb964b69dfb55b0c50c91d817
lestat <lestat@lestat.me>
2019-03-02 16:11:07 +0800

    更新了频率限制规则

--- openresty/access_limit_by_specific_rules.lua
+++ openresty/access_limit_by_specific_rules.lua
@@ -8,35 +8,18 @@ local function close_redis(red)
     local ok, err = red:set_keepalive(pool_max_idle_time, pool_size)
 
     if not ok then
-        ngx_log(ngx_ERR, "set redis keepalive error : ", err)
+        ngx.log(ngx.ERR, "set redis keepalive error : ", err)
     end
 end
-
---[[
-    table是否包含指定值
-]]
-local function has_value (tab, val)
-    for index, value in ipairs(tab) do
-        if value == val then
-            return true
-        end
-    end
-
-    return false
-end
-
-local cjson = require 'cjson'
-
-ngx.log(ngx.DEBUG, '1')
 
 --[[
     当前策略:
-    请求可能携带token或直接访问;对直接访问的请求通过ip识别身份,配置一个较高的每秒频率,超过频率后对ip进行全局封锁至指定时间;对携带token访问的请求通过token识别身份,设置一个较低的频率,超过频率后对当前所请求的uri进行限制访问至指定时间
+    请求可能携带token或直接访问;对直接访问的请求通过ip识别身份(但一个ip可能对应多个client),因此配置一个较高的每秒频率,超过频率后对ip进行全局forbidden至指定时间;对携带token访问的请求通过token识别身份(通常是同一个client),设置一个较低的频率,超过频率后对该token和其所在ip进行全局forbidden至指定时间
 ]]
 -- ip最大频率
-local ipMaxFreq = 50
+local ipMaxFreq = 9
 -- token最大频率
-local tokenMaxFreq = 10
+local tokenMaxFreq = 9
 -- 超过阈值后被ban时间
 local banExpire = 600
 
@@ -54,15 +37,6 @@ if not ok then
 end
 
 --[[
-    reused times
-]]
-local times, err = red:get_reused_times()
-
-if times ~= nil then
-    ngx.log(ngx.DEBUG, 'times:'..times)
-end
-
---[[
     优先判断是否存在token
 ]]
 --token名称,此处根据实际情况修改
@@ -70,64 +44,77 @@ local token = "Authorization"
 
 clientToken = ngx.req.get_headers()[token]
 
-local limited_paths = {'/asd', '/aaa'}
+--[[
+    获取客户端真实IP
+]]
+local clientIP = ngx.req.get_headers()["X-Real-IP"]
+if clientIP == nil then
+    clientIP = ngx.req.get_headers()["x_forwarded_for"]
+end
+if clientIP == nil then
+    clientIP = ngx.var.remote_addr
+end
+
+-- ngx.log(ngx.ERR, clientToken)
 
 if clientToken ~= nil then
-    local incrKey = "user:"..clientToken..ngx.var.request_uri..":freq"
-    local blockKey = "user:"..clientToken..ngx.var.request_uri..":block"
+    local incrKey = "user:"..clientToken..":freq"
+    local tokenBlockKey = "userToken:"..clientToken..":block"
+    local ipBlockKey = "userIp:"..clientIP..":block"
 
     --[[
         判断是否被ban
     ]]
-    local is_block,err = red:get(blockKey) -- check if ip is blocked
+    local is_block,err = red:get(tokenBlockKey) -- check if ip is blocked
     if tonumber(is_block) == 1 then
-    ngx.exit(ngx.HTTP_FORBIDDEN)
-    return close_redis(red)
+        ngx.exit(ngx.HTTP_FORBIDDEN)
+        return close_redis(red)
     end
 
-    if has_value(limited_paths, ngx.var.request_uri) == true then
-        --[[
-        每秒访问频率+1
-        ]]
-        res, err = red:incr(incrKey)
+    local is_block,err = red:get(ipBlockKey) -- check if ip is blocked
+    if tonumber(is_block) == 1 then
+        ngx.exit(ngx.HTTP_FORBIDDEN)
+        return close_redis(red)
+    end
 
-        --[[
-            上一步操作成功,则为当前key设置过期时间
-        ]]
-        if res == 1 then
+    --[[
+    每秒访问频率+1
+    ]]
+    res, err = red:incr(incrKey)
+
+    --[[
+        上一步操作成功,则为当前key设置过期时间
+    ]]
+    if res == 1 then
         res, err = red:expire(incrKey,1)
-        end
+    end
 
-        --[[
-            每秒请求数大于阈值,屏蔽指定值(秒)
-        ]]
-        if res > tokenMaxFreq then
-            res, err = red:set(blockKey,1)
-            res, err = red:expire(blockKey,banExpire)
-        end
+    --[[
+        每秒请求数大于阈值,屏蔽指定值(秒)
+    ]]
+    if res > tokenMaxFreq then
+        -- ban token
+        res, err = red:set(tokenBlockKey,1)
+        res, err = red:expire(tokenBlockKey,banExpire)
+
+        -- ban ip
+        res, err = red:set(ipBlockKey,1)
+        res, err = red:expire(ipBlockKey,banExpire)
+
+        -- ngx.log(ngx.ERR, tokenBlockKey)
+        -- ngx.log(ngx.ERR, ipBlockKey)
     end
 else
-    --[[
-        获取客户端真实IP
-    ]]
-    local clientIP = ngx.req.get_headers()["X-Real-IP"]
-    if clientIP == nil then
-        clientIP = ngx.req.get_headers()["x_forwarded_for"]
-    end
-    if clientIP == nil then
-        clientIP = ngx.var.remote_addr
-    end
-
     local incrKey = "user:"..clientIP..":freq"
-    local blockKey = "user:"..clientIP..":block"
+    local blockKey = "userIp:"..clientIP..":block"
 
     --[[
         判断是否被ban
     ]]
     local is_block,err = red:get(blockKey) -- check if ip is blocked
     if tonumber(is_block) == 1 then
-    ngx.exit(ngx.HTTP_FORBIDDEN)
-    return close_redis(red)
+        ngx.exit(ngx.HTTP_FORBIDDEN)
+        return close_redis(red)
     end
 
     --[[
--- www/index.php
+++ www/index.php
@@ -1,2 +1,2 @@
 <?php
-phpinfo();
+die('666');
